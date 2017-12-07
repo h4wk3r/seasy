7643cf9db132c6f66fb21126e2dfb6515e4d513a
vdb <vdbnicolas@gmail.com>
2017-12-07 14:15:10 +0100

    Add files via upload

---
+++ seasy.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+## COLOR
+GREEN="\\033[1;32m"
+RED="\\033[1;31m"
+BLUE="\\033[1;34m"
+NC='\033[0m'
+
+#DIR CREATE FILE
+file_conf='/var/opt/nagios'
+file_nrpe='/etc/nagios'
+dir_pluggin='/etc/nagios-plugins'
+
+
+## HEADER
+head()
+{
+        echo
+        echo  "  ---------------NICO-BART-FLO---------------"
+        echo
+}
+
+
+## VERIFICATION DES DEPENDANCES AVANT INSTALLATION
+check_base()
+{
+    echo " "
+    echo -e "${BLUE}Analyses système : ${NC}"
+    # Check OS Type
+    os=$(uname -o)
+    echo -e '\E[32m'"Operating System Type : \033[0m"$os
+
+    # Check OS Release Version and Name
+    os_name=$(uname -n)
+    echo -e '\E[32m'"OS Name : \033[0m"$os_name
+
+    os_version=$(uname -v)
+    echo -e '\E[32m'"OS Version : \033[0m" $os_version
+
+    # Check Architecture
+    architecture=$(uname -m)
+    echo -e '\E[32m'"Architecture : \033[0m"$architecture
+
+    # Check Kernel Release
+    kernelrelease=$(uname -r)
+    echo -e '\E[32m'"Kernel Release : \033[0m"$kernelrelease
+
+    # Check hostname
+    echo -e '\E[32m'"Hostname : \033[0m"$HOSTNAME
+}
+
+
+#MAIN
+head
+check_base
+
+exit 0
+
