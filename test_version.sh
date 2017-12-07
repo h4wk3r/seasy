c7faa6e4f671009ba5320f2df02e4ad3068edbfc
barthelemypousset <barthelemypousset@gmail.com>
2017-12-07 17:17:40 +0100

    updated test_version

--- test_version.sh
+++ test_version.sh
@@ -1,5 +1,18 @@
-if [[ -f /etc/debian_version || -f /etc/redhat_release ]]  
-then echo "present" 
-else echo "absent"
+if [ -f /etc/debian_version ]
+   then
+       echo "Fichier de distribution debian trouvé" 
+       DISTRIBUTION="Debian $(cat /etc/debian_version)"
+elif [ -f /etc/redhat-release ] 
+   then
+       echo "Fichier de distribution redhat trouvé"
+       DISTRIBUTION=$(cat /etc/redhat-release)
+   else
+       echo "Fichier de distribution absent"
 fi
+
+echo "architecture : $(uname -m)"
+echo "distribution : $DISTRIBUTION"
+echo "kernel : $(uname -s)"
+echo "kernel release : $(uname -r)"
+echo "kernel version : $(uname -v)"
 
