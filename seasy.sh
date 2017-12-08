54c6249911c6a2955e1d18ff70e3e7b288e2d4fe
root <root@labolinux>
2017-12-08 14:01:43 +0100

    h4wk3r - 08/12

--- seasy.sh
+++ seasy.sh
@@ -6,34 +6,31 @@ RED="\\033[1;31m"
 BLUE="\\033[1;34m"
 NC='\033[0m'
 
-
 check_distribution()
 {
-    if [ -f /etc/debian_version ]
+       if [ -f /etc/debian_version ]
        then
-           DISTRIBUTION="Debian $(cat /etc/debian_version)"
-    elif [ -f /etc/redhat-release ] 
-       then
-           DISTRIBUTION=$(cat /etc/redhat-release)
-    else
-       DISTRIBUTION="Fichier de distribution absent" 
-    fi
-    echo -e "${GREEN}OS Name : \033[0m"$(uname -n)    
-    echo -e "${GREEN}Achitecture :\033[0m" $(uname -m)
-    echo -e "${GREEN}Kernel :\033[0m" $(uname -s)
-    echo -e "${GREEN}Kernel release :\033[0m" $(uname -r)
-    echo -e "${GREEN}Date :\033[0m" $(date | awk '{print $1, $2, $3, $4, $5}')
-    echo -e "${GREEN}Uptime :\033[0m" $(uptime | awk '{print $1}')
-    echo ""
-    echo -e "${GREEN}Utilisateur(s) connecté(s) :\033[0m" 
-    echo $(who | awk '{print $1}' | sort | uniq)
+                DISTRIBUTION="Debian $(cat /etc/debian_version)"
+        elif [ -f /etc/redhat-release ] 
+        then
+		DISTRIBUTION=$(cat /etc/redhat-release)
+	else
+		DISTRIBUTION="Fichier de distribution absent" 
+	fi
+	echo -e "${GREEN}OS Name : \033[0m"$(uname -n)    
+	echo -e "${GREEN}Achitecture :\033[0m" $(uname -m)
+	echo -e "${GREEN}Kernel :\033[0m" $(uname -s)
+	echo -e "${GREEN}Kernel release :\033[0m" $(uname -r)
+	echo -e "${GREEN}Date :\033[0m" $(date | awk '{print $1, $2, $3, $4, $5}')
+	echo -e "${GREEN}Uptime :\033[0m" $(uptime | awk '{print $1}')
+	echo ""
+	echo -e "${GREEN}Utilisateur(s) connecté(s) :\033[0m" 
+	echo $(who | awk '{print $1}' | sort | uniq)
 
 }
 
-
-
 check_ip()
-{	
+{
 	echo -e "${GREEN}Interface Machine : ${NC}"
 	ip -o addr | awk '!/^[0-9]*: ?lo|link\/ether/ {gsub("/", " "); print $2" "$4}'
 }
@@ -56,13 +53,14 @@ check_fs()
 	check_fdisk
 	df -h | awk ' {print $6,":  " $5,"sur " $2 " disponible"} '  | grep -E "/dev|/boot|/etc|/home"
 }
+
 check_fdisk()
 {
 	#echo -e "${GREEN}Ensemble des disks : ${NC}"
 	fdisk -l | grep -E "Disque /dev/s|Disk /dev/s" | cut -d "," -f 1
 }
 
-echo""
+echo ""
 check_distribution
 echo ""
 check_ip
@@ -73,5 +71,6 @@ check_ram
 echo ""
 check_fs
 echo ""
+
 exit 0
 
